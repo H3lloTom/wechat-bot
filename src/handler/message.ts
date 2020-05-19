8d961a9c3ef6b20e3e5458f0b73e2ac35ff2b289
xuyanqi <963945230@qq.com>
2020-05-19 17:37:09 +0800

    feat: 模板编写

--- src/handler/message.ts
+++ src/handler/message.ts
@@ -1,15 +1,25 @@
-import { Message, Wechaty, Contact } from "wechaty";
+import { Message, Wechaty, Contact } from 'wechaty';
 
-const helperTmp = () => 
-` --帮助手册--
+const helperTmp = () =>
+  ` --帮助手册--
   回复【订单】查看最近购买记录
   回复【余额】查看当前余额
   回复【提现】提现购物的返利余额
 `;
 
-const orderTmp = () => ``;
+const orderTmp = () =>
+  `
+  --订单信息--
+  最近订单成交数：
+  最近邀请好友订单成交数：
+`;
 
-const balanceTmp = () => ``;
+const balanceTmp = () =>
+  `
+  --余额信息--
+  余额：
+  提现记录：
+`;
 
 const extractTmp = () => ``;
 
@@ -31,14 +41,22 @@ export default async function handler(this: Wechaty, msg: Message) {
 
   if (contact.type())
     switch (text) {
-      case "帮助":
-        msg.say(helperTmp());
+      case '帮助':
+        setTimeout(() => {
+          msg.say(helperTmp());
+        }, 1000);
         return;
-      case "订单":
+      case '订单':
+        setTimeout(() => {
+          msg.say(orderTmp());
+        }, 1000);
         return;
-      case "余额":
+      case '余额':
+        setTimeout(() => {
+          msg.say(balanceTmp());
+        }, 1000);
         return;
-      case "提现":
+      case '提现':
         return;
       default:
         break;
